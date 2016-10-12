696c7b4d5a0bf6cb08d5944889150118468cd427
Mayank <mayankamencherla24@gmail.com>
2016-10-12 17:50:37 +0530

    php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -100,6 +100,7 @@ $app->group(['namespace' => 'App\Http\Controllers'], function ($app) {
 // LOADING THE REDIS CONFIG
 if (getenv('REDIS_URL')) {
     $url = parse_url(getenv('REDIS_URL'));
+    print_r(array_keys($url));
     echo $url['host'] . $url['port'];
     putenv('REDIS_HOST='.$url['host']);
     putenv('REDIS_PORT='.$url['port']);
